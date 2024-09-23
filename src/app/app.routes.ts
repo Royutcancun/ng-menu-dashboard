56c16459a734b62d9f03ce6aba103b69a7ee40a6
Rogelio Lopez <royutcancun@gmail.com>
2024-09-23 15:59:11 -0500

    Sidebar fixed & routing review

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 
-/*export const routes: Routes = [
+export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./shared/components/layout/layout.component'),
@@ -30,12 +30,16 @@ import { Routes } from '@angular/router';
         redirectTo: 'dashboard'
     }
 ];
-*/
+/*
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./shared/components/layout/layout.component').then(m => m.LayoutComponent), children: [
         { path: 'dashboard', loadComponent: () => import('./business/dashboard/dashboard.component').then(m => m.DashboardComponent) },
         { path: 'profile', loadComponent: () => import('./business/profile/profile.component').then(m => m.ProfileComponent) },
-        { path: 'tables', loadComponent: () => import('./business/tables/tables.component').then(m => m.TablesComponent) }
-    ] },
+        { path: 'tables', loadComponent: () => import('./business/tables/tables.component').then(m => m.TablesComponent) },
+        { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+    ]
+    },
+    { path: '**', redirectTo: 'dashboard' }
     
 ]
+    */
--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -11,6 +11,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+export default class LayoutComponent {
 
 }
